30d711085c7d6c9dca2bb3501837cde3f9288586
John Nario <johnnario@Johns-MacBook-Air.local>
2025-10-23 11:50:01 -0400

    call_home

--- ros2_ws/src/cpmr_ch12/cpmr_ch12/yolo_pose.py
+++ ros2_ws/src/cpmr_ch12/cpmr_ch12/yolo_pose.py
@@ -26,146 +26,7 @@ from kortex_api.autogen.messages import Base_pb2, BaseCyclic_pb2, Common_pb2
 
 from cpmr_ch12.utilities import parseConnectionArguments, DeviceConnection
 
-
-# Maximum allowed waiting time during actions (in seconds)
-TIMEOUT_DURATION = 20
-
-# Create closure to set an event after an END or an ABORT
-def check_for_end_or_abort(e):
-    """Return a closure checking for END or ABORT notifications
-
-    Arguments:
-    e -- event to signal when the action is completed
-        (will be set when an END or ABORT occurs)
-    """
-    def check(notification, e = e):
-        print("EVENT : " + \
-              Base_pb2.ActionEvent.Name(notification.action_event))
-        if notification.action_event == Base_pb2.ACTION_END \
-        or notification.action_event == Base_pb2.ACTION_ABORT:
-            e.set()
-    return check
-
-
-def example_angular_action_movement(base, angles=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]):
-
-    print("Starting angular action movement ...")
-    action = Base_pb2.Action()
-    action.name = "Example angular action movement"
-    action.application_data = ""
-
-    actuator_count = base.GetActuatorCount()
-
-    # Place arm straight up
-    print(actuator_count.count)
-    if actuator_count.count != len(angles):
-        print(f"bad lengths {actuator_count.count} {len(angles)}")
-    for joint_id in range(actuator_count.count):
-        joint_angle = action.reach_joint_angles.joint_angles.joint_angles.add()
-        joint_angle.joint_identifier = joint_id
-        joint_angle.value = angles[joint_id]
-
-    e = threading.Event()
-    notification_handle = base.OnNotificationActionTopic(
-        check_for_end_or_abort(e),
-        Base_pb2.NotificationOptions()
-    )
-
-    print("Executing action")
-    base.ExecuteAction(action)
-
-    print("Waiting for movement to finish ...")
-    finished = e.wait(TIMEOUT_DURATION)
-    base.Unsubscribe(notification_handle)
-
-    if finished:
-        print("Angular movement completed")
-    else:
-        print("Timeout on action notification wait")
-    return finished
-
-def example_move_to_home_position(base):
-    # Make sure the arm is in Single Level Servoing mode
-    base_servo_mode = Base_pb2.ServoingModeInformation()
-    base_servo_mode.servoing_mode = Base_pb2.SINGLE_LEVEL_SERVOING
-    base.SetServoingMode(base_servo_mode)
-
-    # Move arm to ready position
-    print("Moving the arm to a safe position")
-    action_type = Base_pb2.RequestedActionType()
-    action_type.action_type = Base_pb2.REACH_JOINT_ANGLES
-    action_list = base.ReadAllActions(action_type)
-    action_handle = None
-    for action in action_list.action_list:
-        if action.name == "Home":
-            action_handle = action.handle
-
-    if action_handle == None:
-        print("Can't reach safe position. Exiting")
-        return False
-
-    e = threading.Event()
-    notification_handle = base.OnNotificationActionTopic(
-        check_for_end_or_abort(e),
-        Base_pb2.NotificationOptions()
-    )
-
-    base.ExecuteActionFromReference(action_handle)
-    finished = e.wait(TIMEOUT_DURATION)
-    base.Unsubscribe(notification_handle)
-
-    if finished:
-        print("Safe position reached")
-    else:
-        print("Timeout on action notification wait")
-    return finished
-
-
-def get_angular_state(base_cyclic):
-    feedback = base_cyclic.RefreshFeedback()
-    actuators = feedback.actuators
-    v = []
-    for j in actuators:
-        v.append(j.position)
-    return v
-
-def example_cartesian_action_movement(base, x, y, z, theta_x, theta_y, theta_z):
-
-    print("Starting Cartesian action movement ...")
-    action = Base_pb2.Action()
-    action.name = "Example Cartesian action movement"
-    action.application_data = ""
-
-    cartesian_pose = action.reach_pose.target_pose
-    cartesian_pose.x = x
-    cartesian_pose.y = y
-    cartesian_pose.z = z
-    cartesian_pose.theta_x = theta_x
-    cartesian_pose.theta_y = theta_y
-    cartesian_pose.theta_z = theta_z
-
-    e = threading.Event()
-    notification_handle = base.OnNotificationActionTopic(
-        check_for_end_or_abort(e),
-        Base_pb2.NotificationOptions()
-    )
-
-    print("Executing action")
-    base.ExecuteAction(action)
-
-    print("Waiting for movement to finish ...")
-    finished = e.wait(TIMEOUT_DURATION)
-    base.Unsubscribe(notification_handle)
-
-    return finished
-
-def get_tool_state(base_cyclic):
-    feedback = base_cyclic.RefreshFeedback()
-    base = feedback.base
-
-    return  base.tool_pose_x, base.tool_pose_y, base.tool_pose_z, base.tool_pose_theta_x, base.tool_pose_theta_y, base.tool_pose_theta_z
-
-
+from kinova_gen3_node import Kinova_Gen3_Interface
 
 class YOLO_Pose(Node):
     _BODY_PARTS = ["NOSE", "LEFT_EYE", "RIGHT_EYE", "LEFT_EAR", "RIGHT_EAR", "LEFT_SHOULDER", "RIGHT_SHOULDER",
@@ -174,6 +35,7 @@ class YOLO_Pose(Node):
     def __init__(self):
         super().__init__('pose_node')
 
+        self._kinova_interface = Kinova_Gen3_Interface()
         # params
         self._model_file = os.path.join(get_package_share_directory('cpmr_ch12'), 'yolov8n-pose.pt') 
         self.declare_parameter("model", self._model_file) 
@@ -263,55 +125,46 @@ class YOLO_Pose(Node):
                 cv2.imshow('Results', annotated_frame)
                 cv2.waitKey(1)
 
-    # Kinova functions
-    def _handle_home(self, request, response):
-        """Move to home"""
-        self.get_logger().info(f'{self.get_name()} moving to home')
+    def call_home(self):
+        """Call home by moving to a specific Cartesian position"""
+        self.get_logger().info('Moving to home position...')
+        
+        # Define your custom home position in Cartesian space
+        # x, y, z in meters, angles in degrees
+        home_x = 0.4
+        home_y = 0.0
+        home_z = 0.4
+        home_theta_x = 180.0
+        home_theta_y = 0.0
+        home_theta_z = 90.0
+        
+        return self.call_set_tool(home_x, home_y, home_z, 
+                                home_theta_x, home_theta_y, home_theta_z)
 
-        response.status = example_move_to_home_position(self._base)
-        return response
-
-    def _handle_set_joints(self, request, response):
-        """Set joint values"""
-        self.get_logger().info(f'{self.get_name()} Setting joint values')
-        if len(request.joints) != 6:
-            self.get_logger().info(f'{self.get_name()} Must specify exactly six joint angles')
-            response.status = False
-            return response
-
-        response.status = example_angular_action_movement(self._base, angles=request.joints)
-        return response
-    
-    def _handle_get_joints(self, request, response):
-        """Get joint values"""
-        self.get_logger().info(f'{self.get_name()} Getting joint values')
-        response.joints = get_angular_state(self._base_cyclic)
-        return response
-
-
-    def _handle_set_tool(self, request, response):
-        """Set tool values"""
-        self.get_logger().info(f'{self.get_name()} Setting tool values')
-
-        response.status = example_cartesian_action_movement(self._base, request.x, request.y, request.z, request.theta_x, request.theta_y, request.theta_z)
-        return response
-
-    def _handle_get_tool(self, request, response):
-        """Get tool values"""
-        self.get_logger().info(f'{self.get_name()} Getting tool values')
-        x, y, z, theta_x, theta_y, theta_z = get_tool_state(self._base_cyclic)
-        response.x = x
-        response.y = y
-        response.z = z
-        response.theta_x = theta_x
-        response.theta_y = theta_y
-        response.theta_z = theta_z
-        return response
-
+    def call_set_tool(self, x, y, z, theta_x, theta_y, theta_z):
+            """Call the set_tool service"""
+            request = SetTool.Request()
+            request.x = float(x)
+            request.y = float(y)
+            request.z = float(z)
+            request.theta_x = float(theta_x)
+            request.theta_y = float(theta_y)
+            request.theta_z = float(theta_z)
+            
+            future = self._set_tool_client.call_async(request)
+            rclpy.spin_until_future_complete(self, future)
+            
+            if future.result() is not None:
+                self.get_logger().info(f'SetTool returned: {future.result().status}')
+                return future.result().status
+            else:
+                self.get_logger().error('SetTool service call failed')
+                return False
 
 def main(args=None):
     rclpy.init(args=args)
     node = YOLO_Pose()
+
     try:
         rclpy.spin(node)
         rclpy.shutdown()
