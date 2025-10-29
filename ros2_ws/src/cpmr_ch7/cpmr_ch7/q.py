fb6920fad80f7a250757db51f108385cf0dd0279
John Nario <johnnario@Johns-MacBook-Air.local>
2025-10-29 09:01:33 -0400

    7x7 world

--- ros2_ws/src/cpmr_ch7/cpmr_ch7/q.py
+++ ros2_ws/src/cpmr_ch7/cpmr_ch7/q.py
@@ -6,10 +6,10 @@ n_image = 1000
 def save_Q(Q):
     global n_image
 #    print(Q)
-    image = np.zeros((500,500))
-    for idx in range(25):
-        i = int(idx / 5)
-        j = idx % 5
+    image = np.zeros((700,700))
+    for idx in range(49):
+        i = int(idx / 7)
+        j = idx % 7
         v = max(Q[idx])
         z = (v + 1000) / 1500
         if z < 0:
@@ -33,9 +33,9 @@ def execute_action(maze, s, a):
     sp[0] = s[0] + a[0]
     sp[1] = s[1] + a[1]
 
-    isp = sp[0] * 5 + sp[1]
-    idxs = s[0] * 5 + s[1]
-    if (sp[0] < 0) or (sp[1]<0) or (sp[0] >= 5) or (sp[1] >= 5):
+    isp = sp[0] * 7 + sp[1]
+    idxs = s[0] * 7 + s[1]
+    if (sp[0] < 0) or (sp[1]<0) or (sp[0] >= 7) or (sp[1] >= 7):
         return idxs, -100, False
     if maze[sp[0]][sp[1]] == 0:
         return isp, -1, False
@@ -52,7 +52,7 @@ def execute_action(maze, s, a):
 #
 def init_Q():
     Q = []
-    for i in range(25):
+    for i in range(49):
         x = [0, 0, 0, 0]
         Q.append(x)
     return Q
@@ -61,9 +61,9 @@ def reset():
      return 0
 
 def state_to_row_col(s):
-     return [int(s / 5), s % 5]
+     return [int(s / 7), s % 7]
 
-def q_learning(maze, actions, n_episodes=300, max_episode_length=50, epsilon=0.1, alpha=0.2, gamma=0.7):
+def q_learning(maze, actions, n_episodes=500, max_episode_length=100, epsilon=0.1, alpha=0.2, gamma=0.7):
     rewards = []
 
     Q = init_Q()
@@ -91,11 +91,13 @@ def q_learning(maze, actions, n_episodes=300, max_episode_length=50, epsilon=0.1
 # define the maze 0 = unoocuped, 1 = wall, 10 = goal
 #
 maze = np.array([
-    [0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 0],
-    [1, 0, 0, 0, 0],
-    [1, 1, 1, 1, 0],
-    [1, 10, 0, 0, 0]])
+    [0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 1, 0, 1, 0],
+    [1, 0, 0, 0, 0, 1, 0],
+    [1, 0, 1, 1, 1, 1, 0],
+    [1, 10, 0, 0, 0, 0, 0]])
 
 actions = [[0,1], [1,0], [0,-1], [-1, 0]]
 
